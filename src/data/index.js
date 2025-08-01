a32610ab209661319c8eb4f07868f2175d064162
Dalia Salcedo <salc0015@algonquinlive.com>
2025-07-31 23:37:37 -0400

    Creates files, add contacts to index.js

---
+++ src/data/index.js
@@ -0,0 +1,32 @@
+export const contacts = [
+    {
+        id: 1,
+        firstName: "Alice",
+        lastName: "Johnson",
+        email: "alice@gmail.com"
+    },
+    {
+        id: 2,
+        firstName: "Bob",
+        lastName: "Smith",
+        email: "bob@gmail.com"
+    },
+    {
+        id: 3,
+        firstName: "Carol",
+        lastName: "White",
+        email: "carol@gmail.com"
+    },
+    {
+        id: 4,
+        firstName: "David",
+        lastName: "Brown",
+        email: "david@gmail.com"
+    },
+    {
+        id: 5,
+        firstName: "Eva",
+        lastName: "Davis",
+        email: "eva@gmail.com"
+    }
+]
